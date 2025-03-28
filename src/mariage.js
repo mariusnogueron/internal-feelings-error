b6d529c51caabe883459b2dfa3fe015101500ac8
theo-gambier <contact@theo-gambier.fr>
2025-03-28 14:22:11 +0100

    corrections MEP

--- src/mariage.js
+++ src/mariage.js
@@ -11,6 +11,7 @@ const popup = document.getElementById('popup')
 if ((alwaysGood !== 'true' && lcBad > lcGood) || alwaysBad ==='true' ){
   document.getElementById('imageMariage').src = 'assets/imgs/mariage-collage.jpg'
   document.getElementById('pPopup').innerText = 'Parfois il vaut mieux laisser les choses se faire …'
+  document.getElementById('fondMariage').classList.remove('hidden')
 }
 
 buttonPopup.addEventListener('click', ()=>{
